caf92d43cb5e4a02887e73b933ef2713e3a4918e
erik tolentino <tolentino3575@gmail.com>
2016-06-08 15:10:29 -0700

    Make progress on slider, still not functioning correctly

--- wp-content/themes/Noel/web/scripts/app.js
+++ wp-content/themes/Noel/web/scripts/app.js
@@ -1,11 +1,11 @@
 jQuery(document).foundation();
 
-$(document).ready(function(){
-    $('.imgslider').slick({
+jQuery(document).ready(function(){
+    jQuery('.imgslider').slick({
         infinite: true,
         speed: 300,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 1
     });
 });
