fec91f0d11ebb9fbfad941d15e784b83152731ff
Robert M. Parker <rparker@yamiko.ninja>
2016-06-24 12:56:24 -0700

    fix template issue

--- wp-content/themes/Noel/index.php
+++ wp-content/themes/Noel/index.php
@@ -1,4 +1,13 @@
 <?php
 require_once __DIR__.'/App/bootstrap.php';
+global $container;
+
+$data = [];
+
+if(is_front_page()) {
+    $template = 'base.html.twig';
+} else {
+    $template = 'base.html.twig';
+}
 
 $twig = $container->get("twig.environment");
