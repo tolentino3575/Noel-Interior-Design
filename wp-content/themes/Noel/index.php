fcb88133a6ce130e28def6556a3c74bfaad66260
Robert M. Parker <rparker@yamiko.ninja>
2016-06-24 06:48:20 -0700

    update base template and some basic refactoring

--- wp-content/themes/Noel/index.php
+++ wp-content/themes/Noel/index.php
@@ -1,9 +1,4 @@
 <?php
 require_once __DIR__.'/App/bootstrap.php';
 
-    ob_start();
-    wp_head();
-    $head = ob_get_clean();
-
 $twig = $container->get("twig.environment");
-echo $twig->render('home.html.twig', array('head' => $head));
--- wp-content/themes/Noel/page-services.php
+++ wp-content/themes/Noel/page-services.php
@@ -1,9 +1,5 @@
 <?php
 require_once __DIR__.'/App/bootstrap.php';
 
-    ob_start();
-    wp_head();
-    $head = ob_get_clean();
-
 $twig = $container->get("twig.environment");
-echo $twig->render('services.html.twig', array('head' => $head));
+echo $twig->render('pages/services.html.twig', []);
